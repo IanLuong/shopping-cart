8546fb0d72d416fd4186150947eefe76ede50fa2
Ian <ianluong@hotmail.co.uk>
2022-06-23 15:25:05 +0100

    Add and amend home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import ImageOne from "../img/julio-rionaldo-C0oRQ_8SxBY-unsplash.jpg"
-import ImageTwo from "../img/karl-hornfeldt-L5gqyA9NLAM-unsplash.jpg"
-import ImageThree from "../img/mitchel-lensink-HPITX4JuuSo-unsplash.jpg"
-import { useEffect, useState } from "react"
+import ImageTwo from "../img/karl-hornfeldt-L5gqyA9NLAM-unsplash.png"
+import ImageThree from "../img/mitchel-lensink-HPITX4JuuSo-unsplash.png"
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
 
     const [currentSlide, setCurrentSlide] = useState(0)
     const homeScreens = [
         {
-            text: "Music on a plate never tasted this good...",
+            text: "Buy some sound on a plate.",
             image: ImageOne
         },
         {
@@ -28,18 +28,28 @@ export default function Home() {
             setCurrentSlide(slide => ((slide + 1) % 3))
         }, 5000)
     }, [currentSlide])
-
+    
     return (
         <div className="carousel">
-            <div className="carousel-item">
-                <h1 className="carousel-text">{homeScreens[currentSlide].text}</h1>
-                <img src={homeScreens[currentSlide].image} alt=""/>
+            <div className={`carousel-item ${currentSlide !== 0 ? "hidden" : ""}`}>
+                <h1 className="carousel-text">{homeScreens[0].text}</h1>
+                <img src={homeScreens[0].image} alt="" />
             </div>
 
-            <div className="carousel-actions">
-                <button id="carousel-button-prev" aria-label="Previous Slidex">{'<'}</button>
-                <button id="carousel-button-next" aria-label="Next Slide">{'>'}</button>
+            <div className={`carousel-item ${currentSlide !== 1 ? "hidden" : ""}`}>
+                <h1 className="carousel-text">{homeScreens[1].text}</h1>
+                <img src={homeScreens[1].image} alt="" />
             </div>
+
+            <div className={`carousel-item ${currentSlide !== 2 ? "hidden" : ""}`}>
+                <h1 className="carousel-text">{homeScreens[2].text}</h1>
+                <img src={homeScreens[2].image} alt="" />
+            </div>
+
+            {/* <div className="carousel-actions">
+                 <button id="carousel-button-prev" aria-label="Previous Slidex">{'<'}</button>
+                 <button id="carousel-button-next" aria-label="Next Slide">{'>'}</button>
+             </div> */}
         </div>
     )
 }
